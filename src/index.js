e7e98903cdd98ad28a729fc818ca8efb9a929ecb
Juansecode <juan.sebastianri28@gmail.com>
2021-12-02 20:01:03 -0500

    Configured our react app with a simple navbar from the components with reactstrap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
+import App from './App.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="Juansecode" />
   </React.StrictMode>,
   document.getElementById('root')
 );
