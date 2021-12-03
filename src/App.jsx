3950549dd5eadc549c70411e236134b1d79c2ef3
Juansecode <juan.sebastianri28@gmail.com>
2021-12-03 02:31:33 -0500

    Exersice 2: React Components Part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import Menu from './components/menu';
+import {dishes} from './shared/dishes.json';
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes
+    };
+  }
+
   render(){
     return (
-      <div className='App'>
+      <div>
         <Navbar dark color='dark'>
           <div className='container'>
             <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu platos={this.state.dishes} />
       </div>
     );
   }
