0855305e663dbfbc097a00432ec1bb4975d4bff1
Juansecode <juan.sebastianri28@gmail.com>
2021-12-24 14:36:34 -0500

    Changes in Main Component

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -50,7 +50,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} onClick={()=>null}/>} />
                     <Route path="/menu/:dishId" component={DishWithId}/>
-                    <Route path="/aboutus" component={()=> <About leaders={this.state.leaders}/>}/>
+                    <Route exact path="/aboutus" component={()=> <About leaders={this.state.leaders}/>}/>
                     <Route exact path='/contactus' component={()=> <ContactUs />} />
                     <Redirect to="/home" />
                 </Switch>
